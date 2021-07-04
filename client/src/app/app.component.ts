ae49487d64028b4a691ba8498dc2ca0b79f69cb3
Michael Walker <michaelwalker846@yahoo.com>
2021-07-04 19:18:34 -0400

    Section 14 - Completed shopping cart in Angular

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ShoppingCartService } from './shopping-cart/shopping-cart.service';
 
 @Component({
   selector: 'app-root',
@@ -8,8 +9,17 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   title = 'Mikenet';
 
-  constructor() {}
+  constructor(private shoppingCartService: ShoppingCartService) {}
 
   ngOnInit(): void {
+    const shoppingCartId = localStorage.getItem('shoppingcart_id');
+    if (shoppingCartId) {
+      this.shoppingCartService.getShoppingCart(shoppingCartId).subscribe(() => {
+        console.log('initialized shopping cart');
+      }, error => {
+        console.log(error);
+      });
+    }
   }
+
 }
