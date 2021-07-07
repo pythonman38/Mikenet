ede2cab123fec77ce929c4765f91b1bf980aed05
Michael Walker <michaelwalker846@yahoo.com>
2021-07-07 10:58:00 -0400

    Section 16 - Implemented Validation on API

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -60,7 +60,7 @@ namespace API.Controllers
         {
             var user = await _userManager.FindUserByClaimsPrincipalWithAddressAsync(HttpContext.User);
 
-            user.Address =  _mapper.Map<AddressDto, Address>(address);
+            user.Address = _mapper.Map<AddressDto, Address>(address);
 
             var result = await _userManager.UpdateAsync(user);
 
@@ -91,6 +91,11 @@ namespace API.Controllers
         [HttpPost("register")]
         public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
+            if (CheckEmailExistsAsync(registerDto.Email).Result.Value)
+            {
+                return new BadRequestObjectResult(new ApiValidationErrorResponse { Errors = new[] { "Email address is in use" } });
+            }
+
             var user = new AppUser
             {
                 DisplayName = registerDto.DisplayName,
